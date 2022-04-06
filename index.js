def89d4b8e33c04b59d3c0a3b272df5d036e93a2
PriyankaGour <priyanka.g@canopas.com>
2022-04-06 16:28:38 +0530

    Change Structure of View

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+
 var bodyparser=require("body-parser")
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,19 +13,19 @@ var bodyParser = require('body-parser');
 const app = express()
 const http = require('http').createServer(app)
 const PORT = process.env.PORT || 3000
-var connection  = require('./database/config');
+var connection  = require('./database/config.js');
+
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-var authRouter = require('./views/Auth/login.ejs');
- 
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
  
 app.use(session({ 
     secret: '123456',
@@ -35,8 +36,7 @@ app.use(session({
  
 app.use(flash());
 app.use(expressValidator());
- 
-app.use('/auth', authRouter);
+
  
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,7 +48,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
@@ -64,10 +64,6 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
    res.send('CRUD Application')
+  // res.render("index")
  })
-
- app.get('/', function(request, response) {
-	// Render login template
-	response.sendFile(path.join(__dirname + '/index.html'));
-});
 
