2ef199825bb169b3c130a31af84739bd8adbb925
PriyankaGour <priyanka.g@canopas.com>
2022-04-16 10:15:08 +0530

    delete operation on product

--- myapp/controller/all_product.js
+++ myapp/controller/all_product.js
@@ -1,4 +1,4 @@
-// const conn= require("../database/config");
+const db= require("../database/config");
 
 // function Product(req,response){
    
@@ -13,3 +13,18 @@
 //     });
 // }
 // module.exports={Product}
+
+function Delete(req,response){
+var product_id= req.params.id;
+ db.query("DELETE fro Product WHERE pid="+product_id,function(err,rows){
+   if(err){
+     req.flash("error",err);
+    //  res.redirect('product/product');
+   }else{
+     req.flash("success","product deleted successfully! pid="+id);
+     console.log("Deleted Successfully")
+     response.redirect('admindashboard/');
+   }
+ });
+}
+module.exports=Delete;
--- myapp/routes/product-route.js
+++ myapp/routes/product-route.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var db  = require('../database/config.js');
+const flash = require('connect-flash');
+router.use(flash());
+var products= require('../controller/all_product.js');
+const {response}=require("express");
 
 router.get('/product-list', function(req, res, next) {
     var sql='SELECT * FROM Product';
+
     db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.render('product/product', { userData: data});
@@ -24,12 +29,35 @@ router.post('/product-add', function(req, res, next) {
    sql = `INSERT INTO Product (product_name) VALUES ("${product_name}")`;
   db.query(sql, function(err, result) {
     if (err) throw err;
-    console.log('record inserted');
-    var msg = "Product inserted Successfully";
-  
-    res.redirect('/product/product',{ alertMsg: msg });
+    console.log('record inserted');  
+    res.render('product/product')
+
   });
 });
+
+//product delete
+router.get("/product-delete/:id?",function(req,response,next){
+  products.Delete(req,response);
+  });
+
+// router.get("/product-delete/:id", function (req, res, next) {
+//   res.render("product/product");
+// });
+
+
+// router.post('/product-delete/:id', function(req, res, next) {
+//   var product_id= req.params.id;
+//  db.query("DELETE fro Product WHERE pid="+product_id,function(err,rows){
+//    if(err){
+//      req.flash("error",err);
+//     //  res.redirect('product/product');
+//    }else{
+//      req.flash("success","product deleted successfully! pid="+id);
+//      console.log("Deleted Successfully")
+//     //  res.redirect('product/product');
+//    }
+//  });
+// });
 
 
 
